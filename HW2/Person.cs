9ce862387537f6973fcdf96d788898d3a637e56a
LinhTran1209 <143068570+LinhTran1209@users.noreply.github.com>
2023-09-17 21:59:10 +0700

    Add files via upload

---
+++ HW2/Person.cs
@@ -0,0 +1,150 @@
+﻿
+using System;
+
+namespace BTVN
+{
+
+    //Tạo class Person
+    class Person
+    {
+        private string name;
+        private string address;
+        private double salary;
+
+        //Phương thức khởi tạo có tham số
+        public Person (string name, string address, double salary)
+        {
+            this.name = name;
+            this.address = address;
+            this.salary = salary;
+        }
+
+        // Phương thức khởi tạo không tham số
+        public Person() { }
+
+
+        // Tạo hàm get set cho thuộc tính
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (name != null)
+                {
+                    name = value;
+                }
+            }
+        }
+
+        public string Address
+        {
+            get { return address; }
+            set
+            {
+                if (address != null)
+                {
+                    address = value;
+                }
+            }
+        }
+
+        public double  Salary
+        {
+            get { return salary; }
+            set
+            {
+                if (10 >= salary && salary >= 0)
+                {
+                    salary = value;
+                }
+            }
+        }
+
+
+
+        // Phương thức nhập thông tin người 
+        public void Input()
+        {
+            while (true)
+            {
+                Console.WriteLine("Nhập tên: ");
+                string name = Console.ReadLine();
+                if (name != "")
+                {
+                    this.name = name;
+                    break;
+                }
+                else Console.WriteLine("Tên không hợp lệ. Nhập lại!");
+            }
+
+            while (true)
+            {
+                Console.WriteLine("Nhập địa chỉ: ");
+                string address = Console.ReadLine();
+                if (address != "")
+                {
+                    this.address = address;
+                    break;
+                }
+
+                else Console.WriteLine("Địa chỉ không hợp lệ. Nhập lại!");
+            }
+
+            while (true)
+            {
+                double x;
+                Console.WriteLine("Nhập lương: ");
+                string salary = Console.ReadLine();
+                if (double.TryParse(salary, out x) && double.Parse(salary) > 0)
+                {
+                    this.salary = double.Parse(salary);
+                    break;
+                }
+                else Console.WriteLine("Tiền lương không hợp lệ. Nhập lại!");
+            }
+
+        }
+
+
+        // Phương thức sắp xếp theo lương tăng dần (bằng thuật toán nổi bọt)
+        public Person[] sortBySalary(Person[] persons)
+        {
+            if (persons[0] == null)
+            {
+                Console.WriteLine("Mảng không có người không thể sắp xếp");
+                return null;
+            }
+            else
+            {
+                for (int i = 0; i < persons.Length; i++)
+                {
+                    for (int j = i + 1; j < persons.Length; j++)
+                    {
+                        if (persons[i].salary > persons[j].salary)
+                        {
+                            Person temp = persons[i];
+                            persons[i] = persons[j];
+                            persons[j] = temp;
+                        }
+                    }
+                }
+                return persons;
+            }
+        }
+
+
+
+
+        // Phương thức hiển thị 1 người
+        public void Print()
+        {
+            Console.WriteLine($"| {name,-20} | {address,20} | {salary,15} |");
+        }
+
+
+
+    }
+
+
+
+}
---
+++ HW2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace BTVN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+
+
+
+            Console.Write("Nhập số lượng người muốn nhập: ");
+            int n = int.Parse(Console.ReadLine());
+
+
+            // Tạo mảng lưu trữ người với n người
+            Person[] persons = new Person[n];
+
+
+            // Nhập thông tin cho n người rồi nhét vào mảng
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Nhập thông tin cho người {i+1}");
+                Person person = new Person();
+                person.Input();
+                persons[i] = person;
+            }
+
+
+
+            // Hiện thị tất cả n người trong mảng
+            Console.WriteLine($"| {"Họ tên",-20} | {"Địa chỉ",20} | {"Lương",15} |");
+            Console.WriteLine("-----------------------------------------------------------------");
+            for (int i = 0; i < n; i++)
+            {
+                persons[i].Print();
+            }
+            
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+            // Sắp xếp mảng theo lương
+            persons = persons[0].sortBySalary(persons);
+            // hiển thị sau khi sắp xếp
+            Console.WriteLine($"| {"họ tên",-20} | {"địa chỉ",20} | {"lương",15} |");
+            Console.WriteLine("-----------------------------------------------------------------");
+            for (int i = 0; i < n; i++)
+            {
+                persons[i].Print();
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
